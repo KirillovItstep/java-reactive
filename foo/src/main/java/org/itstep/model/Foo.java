ee4d41609811cd0ec7c589ea450f641379302184
KirillovItStep <kirillov.malp@gmail.com>
2023-08-24 14:00:14 +0300

    Reactor java

---
+++ foo/src/main/java/org/itstep/model/Foo.java
@@ -0,0 +1,13 @@
+package org.itstep.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Foo {
+
+    private long id;
+    private String name;
+
+}
